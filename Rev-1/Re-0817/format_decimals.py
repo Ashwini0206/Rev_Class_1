bbdd8d799da70a91b055e8a0bb52a02c315d7f51
digvijay <digvijay@g.com>
2025-03-26 18:51:11 +0530

    Done

---
+++ Rev-1/Re-0817/format_decimals.py
@@ -0,0 +1,31 @@
+#number = 7.893466
+#formatted_number = f"{number:.3f}"
+#print(formatted_number)
+
+table=7
+print(f"{table}*1={table}")
+print(f"{table}*2={table*2}")
+print(f"{table}*3={table*3}")
+print(f"{table}*4={table*4}")
+print(f"{table}*5={table*5}")
+print(f"{table}*10={table*10}")
+
+table9 = 9
+print(f"{table9}*1={table9}")
+print(f"{table9}*2={table9*2}")
+print(f"{table9}*5={table9*5}")
+print(f"{table9}*10={table9*10}")
+
+num1 = (input("Enter 1st num"))
+num2 = (input("Enter 2nd num"))
+
+print("Max between num1 and num2 is", max(num1,num2))
+#string upper,lower formatting
+name = "Pramod"
+print(name.upper())
+#print(name.index(a,d))
+
+#to concatinate int and str
+name = "Uma"
+print(name+"1")
+print(name+str(2))
---
+++ Rev-1/Re-0817/math_function_len_type.py
@@ -0,0 +1,17 @@
+a= pow(2,3)
+print(a)
+
+b =abs(-6)
+print(b)
+
+a=66
+print(a)
+
+#how to take input from user
+
+name=input("Enter your name")
+print("Hi", name)
+print(type(name))
+
+
+
